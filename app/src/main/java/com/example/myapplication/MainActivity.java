35fb6482d32d132d7179a54b8a8b7c0e8e2efcfa
stepkos <stepkos@wp.pl>
2021-10-25 16:37:18 +0200

    Colors and levels

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,12 +18,18 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<Button> buttonHandlers;
-    ArrayList<Integer> orderIndexes = new ArrayList<>();
-    Random random = new Random();
-    TextView topText;
-    View background;
-    public int iterator;
+    private ArrayList<Button> buttonHandlers;
+    private ArrayList<Integer> orderIndexes = new ArrayList<>();
+    private final ArrayList<Integer> colors = new ArrayList<>(Arrays.asList(
+       Color.GREEN, Color.RED,
+       Color.YELLOW, Color.BLUE
+    ));
+    private final Random random = new Random();
+    private TextView topText;
+    private View background;
+    private int iterator;
+    private int lvl = 4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +51,12 @@ public class MainActivity extends AppCompatActivity {
         setEventListeners();
     }
 
-    public void setDefaultButtonColors() {
+    private void setDefaultButtonColors() {
         buttonHandlers.forEach(x -> x.setBackgroundColor(Color.GRAY));
     }
 
-    public void setEventListeners() {
-        buttonHandlers.stream().forEach(x -> x.setOnClickListener(view -> {
+    private void setEventListeners() {
+        buttonHandlers.forEach(x -> x.setOnClickListener(view -> {
             if (!orderIndexes.isEmpty()) {
                 if (buttonHandlers.indexOf(view) == orderIndexes.get(0)) {
                     Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
@@ -64,21 +70,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void start(View view) {
 
-        topText.setText("");
+        topText.setText(String.format("Level %s", lvl-3));
         background.setBackgroundColor(Color.WHITE);
         orderIndexes.clear(); // to be sure it is clear
 
         // generate random indexes of handlers to memorise
-        for (int i=0; i<4; i++)
+        for (int i=0; i<lvl; i++)
             orderIndexes.add(random.nextInt(4));
 
         iterator = 0;
-        new CountDownTimer(4000, 1000) {
+        new CountDownTimer(lvl * 1000L, 1000) {
 
             @SuppressLint("ResourceAsColor")
             @Override
             public void onTick(long millisUntilFinished) {
-                buttonHandlers.get(orderIndexes.get(iterator)).setBackgroundColor(Color.BLUE);
+
+                int index = orderIndexes.get(iterator);
+                buttonHandlers.get(index).setBackgroundColor(colors.get(index));
 
                 new Handler().postDelayed(() -> {
                     setDefaultButtonColors();
@@ -94,18 +102,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void won() {
+    @SuppressLint("SetTextI18n")
+    private void won() {
         Toast.makeText(this, "Won!", Toast.LENGTH_SHORT).show();
         orderIndexes.clear();
-        topText.setText("You're won! Click start to play again");
+        topText.setText("You're won! Click start to go to the next lvl");
         background.setBackgroundColor(Color.GREEN);
+        lvl++;
     }
 
-    public void lose() {
+    @SuppressLint("SetTextI18n")
+    private void lose() {
         Toast.makeText(this, "Lose!", Toast.LENGTH_SHORT).show();
         orderIndexes.clear();
         topText.setText("You're lose! Click start to play again");
         background.setBackgroundColor(Color.RED);
+        lvl = 4;
     }
     
 }
